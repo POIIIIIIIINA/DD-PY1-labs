bc8ed69315672691ff4b244b1b0049b5c188ddab
POIIIIIIIINA <bondarevapolina243@gmail.com>
2024-11-24 20:28:57 +0300

    Лабораторная работа 3

---
+++ Лабораторная 3/Передача аргументов в функцию и возвращаемые значения.py
@@ -0,0 +1,11 @@
+# TODO Напишите функцию `multiply_numbers`
+def multiply_numbers(a, b):
+    product = a * b
+    return product
+result1 = multiply_numbers(2, 3)
+result2 = multiply_numbers(4, 5)
+result3 = multiply_numbers(6, 7)
+# TODO Перемножте с помощью функции `multiply_numbers` при пары чисел и распечатайте результаты
+print(result1)
+print(result2)
+print(result3)
